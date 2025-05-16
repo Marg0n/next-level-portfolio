e41f0bf922a70c8ad2f86a1e2c70f2edf78cbcf3
MarGoN <monondcosta@gmail.com>
2025-05-16 23:39:34 +0600

    Modify: Home page changed.

---
+++ src/app/(CommonLayout)/(home)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div className='min-h-screen flex items-center justify-center space-y-6 overflow-x-hidden bg-gradient-to-r from-blue-500 to-purple-500'>
+      Landing page
+    </div>
+  );
+};
+
+export default LandingPage;
