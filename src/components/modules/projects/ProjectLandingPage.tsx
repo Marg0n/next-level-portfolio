bcf35035e69a15a7e40269d1666869c63240693c
MarGoN <monondcosta@gmail.com>
2025-06-27 20:14:17 +0600

    Modify: modified GlassNavbar and ProjectLandingPage to a grid position

--- src/components/modules/projects/ProjectLandingPage.tsx
+++ src/components/modules/projects/ProjectLandingPage.tsx
@@ -12,14 +12,19 @@ const ProjectLandingPage = () => {
     <div className="min-h-[80vh] w-full flex flex-col items-center justify-center space-y-6 overflow-x-hidden ">
       {/* components */}
       {/* <StarfieldWarp> */}
-        this is the project page
-        
-        
-        <AnimatedCardZoomInWithDesc/>
-        <AnimatedCardHoverButtonWithPic/>
-        <AnimatedCardForPerson/>
-        <AnimatedCardFlip/>
-        <AnimatedCardWithImageTilt />
+        <h2 className="mt-4 font-bold text-4xl tracking-tight">
+          <span className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent">
+            Some of My project Lists
+          </span>
+        </h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-6">
+          <AnimatedCardZoomInWithDesc/>
+          <AnimatedCardHoverButtonWithPic/>
+          <AnimatedCardForPerson/>
+          <AnimatedCardFlip/>
+          <AnimatedCardWithImageTilt />
+        </div>
       {/* </StarfieldWarp> */}
     </div>
   );
