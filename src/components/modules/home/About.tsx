77101398d025a500f4c046b826b3e8f761835381
MarGoN <monondcosta@gmail.com>
2025-06-29 20:51:28 +0600

    Modify: Modified About layout.

--- src/components/modules/home/About.tsx
+++ src/components/modules/home/About.tsx
@@ -1,32 +1,59 @@
-import React from 'react';
+import React from "react";
 import aboutImage from "@/assets/images/coding_happy.png";
-import Image from 'next/image';
+import Image from "next/image";
+import whoAmI from "@/assets/images/jacky-who-am-i.png";
 
 const About = () => {
-    return (
-        <div 
-            className='flex flex-col gap-6 w-full p-5 relative' 
-            // style={{backgroundImage: `url(${aboutImage.src})`, backgroundSize: 'contain', backgroundPosition: 'center', backgroundRepeat: 'no-repeat'}}
+  return (
+    <div
+      className="flex flex-col gap-6 w-full p-5 relative"
+      // style={{backgroundImage: `url(${aboutImage.src})`, backgroundSize: 'contain', backgroundPosition: 'center', backgroundRepeat: 'no-repeat'}}
+    >
+      {/* <div className="absolute inset-0 rounded-3xl container mx-auto opacity-60 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"></div> */}
+      <div className="flex flex-row items-center justify-center">
+        <Image
+          src={whoAmI}
+          alt="Profile Pic"
+          width={600}
+          height={600}
+          className="w-20 h-12 object-contain"
+          data-aos="fade-right"
+        />
+        <h2
+          className="text-4xl lg:text-5xl font-bold text-center text-gray-800"
+          data-aos="fade-down"
         >
-            {/* <div className="absolute inset-0 rounded-3xl container mx-auto opacity-60 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"></div> */}
-            <h2 className="text-5xl text-center" data-aos="fade-down">Who Am I?</h2>
+          Who Am I?
+        </h2>
 
-            <div className="flex flex-col lg:flex-row gap-12 items-center">
-                <Image
-                    src={aboutImage}
-                    alt="Profile Pic"
-                    width={500}
-                    height={500}
-                    className="w-60 lg:w-72 xl:w-92 h-60 lg:h-72 xl:h-92 object-contain border-2 rounded-full"
-                     data-aos="fade-right"
-                />
-                <div className="space-y-6 ">
-                    <p className="text-wrap xl:text-xl text-base" data-aos="fade-up">
-                    I am a dedicated web developer with experience in creating dynamic and responsive web applications. 
-                    </p>
-                    <p className="text-wrap xl:text-xl text-base" data-aos="fade-up">
-                        Expert in the <strong className="primary-color">MERN stack</strong>, I possess knowledge of{" "}
-                        {/* <ul>
+        <Image
+          src={whoAmI}
+          alt="Profile Pic"
+          width={600}
+          height={600}
+          className="w-20 h-12 object-contain"
+          data-aos="fade-right"
+        />
+      </div>
+
+      <div className="flex flex-col lg:flex-row gap-12 items-center">
+        <Image
+          src={aboutImage}
+          alt="Profile Pic"
+          width={500}
+          height={500}
+          className="w-60 lg:w-72 xl:w-92 h-60 lg:h-72 xl:h-92 object-contain border-2 rounded-full"
+          data-aos="fade-right"
+        />
+        <div className="space-y-6 ">
+          <p className="text-wrap xl:text-xl text-base" data-aos="fade-up">
+            I am a dedicated web developer with experience in creating dynamic
+            and responsive web applications.
+          </p>
+          <p className="text-wrap xl:text-xl text-base" data-aos="fade-up">
+            Expert in the <strong className="primary-color">MERN stack</strong>,
+            I possess knowledge of{" "}
+            {/* <ul>
                             <li><i className="primary-color">HTML</i></li>
                             <li><i className="primary-color">CSS</i></li>
                             <li><i className="primary-color">Tailwind</i></li>
@@ -41,28 +68,29 @@ const About = () => {
                             <li><i className="primary-color">Mongoose</i></li>
                             <li><i className="primary-color">MySQL</i></li>
                         </ul> */}
-                        <i className="primary-color">HTML</i>,{" "}
-                        <i className="primary-color">CSS</i>,{" "}
-                        <i className="primary-color">Tailwind</i>,{" "}
-                        <i className="primary-color">JavaScript</i>,{" "}
-                        <i className="primary-color">TypeScript</i>,{" "}
-                        <i className="primary-color">React</i>,{" "}
-                        <i className="primary-color">Next.js</i>,{" "}
-                        <i className="primary-color">Firebase</i>,{" "}
-                        <i className="primary-color">Node JS</i>,{" "}
-                        <i className="primary-color">Express JS</i>,{" "}
-                        <i className="primary-color">MongoDB</i>,{" "}
-                        <i className="primary-color">Mongoose</i>, and{" "}
-                        <i className="primary-color">MySQL</i>. 
-                        <br />
-                        <br />
-                        My passion for coding provides a profound sense of fulfillment. Furthermore, I am eager to expand my knowledge of these technologies and explore additional related fields in the future.
-                    </p>
-                </div>
-            </div>
-            
+            <i className="primary-color">HTML</i>,{" "}
+            <i className="primary-color">CSS</i>,{" "}
+            <i className="primary-color">Tailwind</i>,{" "}
+            <i className="primary-color">JavaScript</i>,{" "}
+            <i className="primary-color">TypeScript</i>,{" "}
+            <i className="primary-color">React</i>,{" "}
+            <i className="primary-color">Next.js</i>,{" "}
+            <i className="primary-color">Firebase</i>,{" "}
+            <i className="primary-color">Node JS</i>,{" "}
+            <i className="primary-color">Express JS</i>,{" "}
+            <i className="primary-color">MongoDB</i>,{" "}
+            <i className="primary-color">Mongoose</i>, and{" "}
+            <i className="primary-color">MySQL</i>.
+            <br />
+            <br />
+            My passion for coding provides a profound sense of fulfillment.
+            Furthermore, I am eager to expand my knowledge of these technologies
+            and explore additional related fields in the future.
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default About;
