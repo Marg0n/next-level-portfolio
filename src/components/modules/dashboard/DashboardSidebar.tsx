bcd15e897b2707163a7365d000259dc246bbe102
MarGoN <monondcosta@gmail.com>
2025-08-11 23:30:33 +0600

    Fix: Fixed the DashboardSidebar linneup.

--- src/components/modules/dashboard/DashboardSidebar.tsx
+++ src/components/modules/dashboard/DashboardSidebar.tsx
@@ -21,15 +21,18 @@ const DashboardSidebar: React.FC = () => {
   const name = "Sokhorio Margon D' Costa";
 
   return (
-    <aside className="transition-all duration-300 ease bg-white justify-between pt-2 h-full">
-      <div
-        className={`mt-0 ${
-          isCollapse1 ? "px-[20px]" : "px-[10px]"
-        } transition-all duration-300 ease-in-out flex-1`}
-      >
-        {isCollapse1 ? (
-          <div className="flex items-center justify-between">
-            {/* <Image
+    <aside className="transition-all duration-300 ease bg-white justify-between pt-2 h-full flex flex-col">
+      
+      {/* Contents */}
+      <div className="">
+        <div
+          className={`mt-0 ${
+            isCollapse1 ? "px-[20px]" : "px-[10px]"
+          } transition-all duration-300 ease-in-out flex-1`}
+        >
+          {isCollapse1 ? (
+            <div className="flex items-center justify-between">
+              {/* <Image
               src={Pic}
               alt="logo"
               className="w-[130px] cursor-pointer"
@@ -37,165 +40,166 @@ const DashboardSidebar: React.FC = () => {
               height={500}
             /> */}
 
-            {/* title */}
-            <h3 className="text-xl font-serif font-bold">
-              Portfolio Dashboard
-            </h3>
+              {/* title */}
+              <h3 className="text-xl font-serif font-bold">
+                Portfolio Dashboard
+              </h3>
 
-            {/* toggle */}
-            <div className="relative group">
-              <GoSidebarCollapse
-                className="text-[1.5rem] dark:text-[#abc2d3] text-gray-600 cursor-pointer"
-                onClick={() => setIsCollapse1(false)}
-              />
-              <div className="absolute -top-1 right-[-115px] translate-x-[20px] opacity-0 z-[-1] group-hover:translate-x-0 group-hover:opacity-100 group-hover:z-[1] transition-all duration-500">
-                <p className="text-[0.9rem] w-max dark:bg-slate-800 dark:text-[#abc2d3] bg-gray-600 text-secondary rounded px-3 py-[5px]">
-                  Collapse
-                </p>
+              {/* toggle */}
+              <div className="relative group">
+                <GoSidebarCollapse
+                  className="text-[1.5rem] dark:text-[#abc2d3] text-gray-600 cursor-pointer"
+                  onClick={() => setIsCollapse1(false)}
+                />
+                <div className="absolute -top-1 right-[-115px] translate-x-[20px] opacity-0 z-[-1] group-hover:translate-x-0 group-hover:opacity-100 group-hover:z-[1] transition-all duration-500">
+                  <p className="text-[0.9rem] w-max dark:bg-slate-800 dark:text-[#abc2d3] bg-gray-600 text-secondary rounded px-3 py-[5px]">
+                    Collapse
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-        ) : (
-          <Image
-            src={Pic}
-            alt="logo"
-            className="w-20 h-20 mx-auto rounded-full cursor-pointer object-contain border-2 border-indigo-400 bg-green-200"
-            onClick={() => setIsCollapse1(!isCollapse1)}
-            width={500}
-            height={500}
-          />
-        )}
-
-        {/* Search bar */}
-        {isCollapse1 ? (
-          <div className="relative mt-5">
-            <input
-              className="px-4 py-2 dark:border-slate-700 dark:bg-transparent dark:text-[#abc2d3] dark:placeholder:text-slate-500 border border-border rounded-md w-full pl-[40px] outline-none focus:border-[#3B9DF8]"
-              placeholder="Search..."
+          ) : (
+            <Image
+              src={Pic}
+              alt="logo"
+              className="w-20 h-20 mx-auto rounded-full cursor-pointer object-contain border-2 border-indigo-400 bg-green-200"
+              onClick={() => setIsCollapse1(!isCollapse1)}
+              width={500}
+              height={500}
             />
-            <IoIosSearch className="absolute top-[9px] left-2 text-[1.5rem] text-[#adadad]" />
-          </div>
-        ) : (
-          <div className="w-full relative group">
-            <IoIosSearch className="text-[2rem] dark:hover:bg-slate-800/50 dark:text-[#abc2d3] mx-auto text-gray-500 mt-2 p-[5px] rounded-md hover:bg-gray-100 cursor-pointer w-full" />
-            <div className="absolute top-0 right-[-85px] translate-x-[20px] opacity-0 z-[-1] group-hover:translate-x-0 group-hover:opacity-100 group-hover:z-[1] transition-all duration-500">
-              <p className="text-[0.9rem] w-max dark:bg-slate-800 dark:text-[#abc2d3] bg-gray-600 text-secondary rounded px-3 py-[5px]">
-                Search
-              </p>
-            </div>
-          </div>
-        )}
-      </div>
+          )}
 
-      {/* options */}
-      {/* General section */}
-      <div
-        className={`mt-6 ${
-          isCollapse1 ? "px-[20px]" : "px-[10px]"
-        } transition-all duration-300 ease-in-out flex-1`}
-      >
-        <div className="mt-3 flex flex-col gap-[5px]">
-          {/* Home */}
-          <SidebarItem
-            isCollapsed={isCollapse1}
-            icon={<GoHome />}
-            label="Home"
-            tooltipOffset="-80px"
-          />
-          {/* Calendar */}
-          <SidebarItem
-            isCollapsed={isCollapse1}
-            icon={<CiCalendar />}
-            label="Calendar"
-            tooltipOffset="-99px"
-          />
-          {/* Projects with dropdown */}
-          <div
-            className={`${isCollapse1 && "justify-center"} ${
-              isDropdownOpen && "bg-gray-50 dark:bg-slate-800"
-            } flex w-full hover:bg-gray-50 p-[5px] dark:hover:bg-slate-800/50 rounded-md cursor-pointer transition-all duration-200 relative group flex-col`}
-            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-          >
-            <div
-              className={`${
-                isCollapse1 ? " justify-between" : "justify-center"
-              } flex items-center gap-[8px] w-full`}
-            >
-              <div className="flex items-center gap-[8px]">
-                <GoProjectSymlink className="text-[1.3rem] dark:text-[#abc2d3] text-gray-500" />
-                <p
-                  className={`${
-                    isCollapse1 ? "inline" : "hidden"
-                  } text-[1rem] font-[400] text-gray-500 dark:text-[#abc2d3]`}
-                >
-                  Projects
+          {/* Search bar */}
+          {isCollapse1 ? (
+            <div className="relative mt-5">
+              <input
+                className="px-4 py-2 dark:border-slate-700 dark:bg-transparent dark:text-[#abc2d3] dark:placeholder:text-slate-500 border border-border rounded-md w-full pl-[40px] outline-none focus:border-[#3B9DF8]"
+                placeholder="Search..."
+              />
+              <IoIosSearch className="absolute top-[9px] left-2 text-[1.5rem] text-[#adadad]" />
+            </div>
+          ) : (
+            <div className="w-full relative group">
+              <IoIosSearch className="text-[2rem] dark:hover:bg-slate-800/50 dark:text-[#abc2d3] mx-auto text-gray-500 mt-2 p-[5px] rounded-md hover:bg-gray-100 cursor-pointer w-full" />
+              <div className="absolute top-0 right-[-85px] translate-x-[20px] opacity-0 z-[-1] group-hover:translate-x-0 group-hover:opacity-100 group-hover:z-[1] transition-all duration-500">
+                <p className="text-[0.9rem] w-max dark:bg-slate-800 dark:text-[#abc2d3] bg-gray-600 text-secondary rounded px-3 py-[5px]">
+                  Search
                 </p>
               </div>
+            </div>
+          )}
+        </div>
 
-              <IoIosArrowDown
+        {/* options */}
+        {/* General section */}
+        <div
+          className={`mt-6 ${
+            isCollapse1 ? "px-[20px]" : "px-[10px]"
+          } transition-all duration-300 ease-in-out flex-1`}
+        >
+          <div className="mt-3 flex flex-col gap-[5px]">
+            {/* Home */}
+            <SidebarItem
+              isCollapsed={isCollapse1}
+              icon={<GoHome />}
+              label="Home"
+              tooltipOffset="-80px"
+            />
+            {/* Calendar */}
+            <SidebarItem
+              isCollapsed={isCollapse1}
+              icon={<CiCalendar />}
+              label="Calendar"
+              tooltipOffset="-99px"
+            />
+            {/* Projects with dropdown */}
+            <div
+              className={`${isCollapse1 && "justify-center"} ${
+                isDropdownOpen && "bg-gray-50 dark:bg-slate-800"
+              } flex w-full hover:bg-gray-50 p-[5px] dark:hover:bg-slate-800/50 rounded-md cursor-pointer transition-all duration-200 relative group flex-col`}
+              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+            >
+              <div
                 className={`${
-                  isDropdownOpen ? "rotate-[180deg]" : "rotate-0"
-                } ${
-                  isCollapse1 ? "inline" : "hidden"
-                } transition-all duration-300 text-[1rem] text-gray-500`}
-              />
+                  isCollapse1 ? " justify-between" : "justify-center"
+                } flex items-center gap-[8px] w-full`}
+              >
+                <div className="flex items-center gap-[8px]">
+                  <GoProjectSymlink className="text-[1.3rem] dark:text-[#abc2d3] text-gray-500" />
+                  <p
+                    className={`${
+                      isCollapse1 ? "inline" : "hidden"
+                    } text-[1rem] font-[400] text-gray-500 dark:text-[#abc2d3]`}
+                  >
+                    Projects
+                  </p>
+                </div>
+
+                <IoIosArrowDown
+                  className={`${
+                    isDropdownOpen ? "rotate-[180deg]" : "rotate-0"
+                  } ${
+                    isCollapse1 ? "inline" : "hidden"
+                  } transition-all duration-300 text-[1rem] text-gray-500`}
+                />
+              </div>
+
+              {!isCollapse1 && (
+                <ul className="translate-y-[20px] opacity-0 z-[-1] group-hover:translate-y-0 group-hover:opacity-100 dark:bg-slate-900 dark:text-[#abc2d3] group-hover:z-30 absolute top-0 left-[70px] bg-white boxShadow transition-all duration-300 p-[8px] rounded-md flex flex-col gap-[3px] text-[1rem] text-gray-500">
+                  <li className="hover:bg-gray-50 dark:hover:bg-slate-800/50 px-[20px] py-[5px] rounded-md">
+                    Google
+                  </li>
+                  <li className="hover:bg-gray-50 dark:hover:bg-slate-800/50 px-[20px] py-[5px] rounded-md">
+                    Facebook
+                  </li>
+                  <li className="hover:bg-gray-50 dark:hover:bg-slate-800/50 px-[20px] py-[5px] rounded-md">
+                    Twitter
+                  </li>
+                  <li className="hover:bg-gray-50 dark:hover:bg-slate-800/50 px-[20px] py-[5px] rounded-md">
+                    Linkedin
+                  </li>
+                </ul>
+              )}
             </div>
 
-            {!isCollapse1 && (
-              <ul className="translate-y-[20px] opacity-0 z-[-1] group-hover:translate-y-0 group-hover:opacity-100 dark:bg-slate-900 dark:text-[#abc2d3] group-hover:z-30 absolute top-0 left-[70px] bg-white boxShadow transition-all duration-300 p-[8px] rounded-md flex flex-col gap-[3px] text-[1rem] text-gray-500">
-                <li className="hover:bg-gray-50 dark:hover:bg-slate-800/50 px-[20px] py-[5px] rounded-md">
-                  Google
-                </li>
-                <li className="hover:bg-gray-50 dark:hover:bg-slate-800/50 px-[20px] py-[5px] rounded-md">
-                  Facebook
-                </li>
-                <li className="hover:bg-gray-50 dark:hover:bg-slate-800/50 px-[20px] py-[5px] rounded-md">
-                  Twitter
-                </li>
-                <li className="hover:bg-gray-50 dark:hover:bg-slate-800/50 px-[20px] py-[5px] rounded-md">
-                  Linkedin
-                </li>
-              </ul>
-            )}
+            {/* Dropdown when expanded */}
+            <ul
+              className={`${
+                isDropdownOpen
+                  ? "h-auto my-3 opacity-100 z-[1]"
+                  : "opacity-0 z-[-1] h-0"
+              } ${
+                isCollapse1 ? "inline" : "hidden"
+              } transition-all duration-300 list-disc marker:text-blue-400 ml-[35px] flex flex-col gap-[3px] text-[1rem] dark:text-[#abc2d3] text-gray-500`}
+            >
+              <li className="hover:bg-gray-50 dark:hover:bg-slate-800/50 px-[10px] py-[5px] rounded-md">
+                Google
+              </li>
+              <li className="hover:bg-gray-50 dark:hover:bg-slate-800/50 px-[10px] py-[5px] rounded-md">
+                Facebook
+              </li>
+              <li className="hover:bg-gray-50 dark:hover:bg-slate-800/50 px-[10px] py-[5px] rounded-md">
+                Twitter
+              </li>
+              <li className="hover:bg-gray-50 dark:hover:bg-slate-800/50 px-[10px] py-[5px] rounded-md">
+                Linkedin
+              </li>
+            </ul>
+
+            {/* Progress & Goals */}
+            <SidebarItem
+              isCollapsed={isCollapse1}
+              icon={<FiBarChart />}
+              label="Progress"
+              tooltipOffset="-100px"
+            />
+            <SidebarItem
+              isCollapsed={isCollapse1}
+              icon={<FiPieChart />}
+              label="Goals"
+              tooltipOffset="-76px"
+            />
           </div>
-
-          {/* Dropdown when expanded */}
-          <ul
-            className={`${
-              isDropdownOpen
-                ? "h-auto my-3 opacity-100 z-[1]"
-                : "opacity-0 z-[-1] h-0"
-            } ${
-              isCollapse1 ? "inline" : "hidden"
-            } transition-all duration-300 list-disc marker:text-blue-400 ml-[35px] flex flex-col gap-[3px] text-[1rem] dark:text-[#abc2d3] text-gray-500`}
-          >
-            <li className="hover:bg-gray-50 dark:hover:bg-slate-800/50 px-[10px] py-[5px] rounded-md">
-              Google
-            </li>
-            <li className="hover:bg-gray-50 dark:hover:bg-slate-800/50 px-[10px] py-[5px] rounded-md">
-              Facebook
-            </li>
-            <li className="hover:bg-gray-50 dark:hover:bg-slate-800/50 px-[10px] py-[5px] rounded-md">
-              Twitter
-            </li>
-            <li className="hover:bg-gray-50 dark:hover:bg-slate-800/50 px-[10px] py-[5px] rounded-md">
-              Linkedin
-            </li>
-          </ul>
-
-          {/* Progress & Goals */}
-          <SidebarItem
-            isCollapsed={isCollapse1}
-            icon={<FiBarChart />}
-            label="Progress"
-            tooltipOffset="-100px"
-          />
-          <SidebarItem
-            isCollapsed={isCollapse1}
-            icon={<FiPieChart />}
-            label="Goals"
-            tooltipOffset="-76px"
-          />
         </div>
       </div>
 
