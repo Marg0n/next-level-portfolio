a00fa40508b38ac0f7ad9270f348374680da84c0
MarGoN <monondcosta@gmail.com>
2025-12-01 23:34:55 +0600

    Modify: Modified EmptyPage page with some changable props.

--- src/components/shared/empty/EmptyPage.tsx
+++ src/components/shared/empty/EmptyPage.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 import React from "react";
 
 type EmptyPageInfo = {
-  title: string;
-  description: string;
+  title?: string;
+  description?: string;
 };
 
-const EmptyPage = () => {
+const EmptyPage = ({ title, description }: EmptyPageInfo) => {
   return (
     <div className="shadow-[0px_0px_10px_0px_rgba(0,0,0,0.1)] dark:bg-slate-900 p-6 sm:px-20 sm:py-14 flex items-center justify-center flex-col gap-[4px] rounded-xl">
       <Image
@@ -18,11 +18,11 @@ const EmptyPage = () => {
       />
 
       <h1 className="text-[1.4rem] dark:text-[#abc2d3] mt-6 font-[500] text-black">
-        No Result
+        {title ?? "No Results Found!"}
       </h1>
 
       <p className="text-[0.9rem] dark:text-slate-400 text-gray-500">
-        Please Come again in the future to view more updates
+        {description ?? "Please Come again in the future to view more updates"}
       </p>
     </div>
   );
