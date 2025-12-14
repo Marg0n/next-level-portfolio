f557017d8a0b4872e70228d6e7a7d0987d7329db
MarGoN <monondcosta@gmail.com>
2025-12-14 19:18:18 +0600

    Modify: Modifing MyBlogs page to fix the iframe of linkedin.

--- src/components/modules/blog/MyBlogs.tsx
+++ src/components/modules/blog/MyBlogs.tsx
@@ -18,33 +18,38 @@ const MyBlogs = () => {
   ];
 
   return (
-    <div className="flex flex-col gap-6 p-6">
-      <h2 className="mt-4 font-bold text-4xl tracking-tight">
-        <span className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent">
-          My LinkedIn Posts
-        </span>
-      </h2>
-      <LinkedInPosts posts={posts} />
-      {/* <iframe
-        src="https://www.linkedin.com/embed/feed/update/urn:li:share:1234567890123456789"
-        height="500"
-        width="100%"
-        frameBorder="0"
-        allowFullScreen
-        title="Exploring Redux: A Game-Changer for State Management in JavaScript Applications"
-      ></iframe> */}
+    <>
+      <div className="flex flex-col justify-center items-center gap-6 p-6 min-h-[80vh] w-full">
+        <h2 className="mt-4 font-bold text-4xl tracking-tight">
+          <span className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent">
+            My LinkedIn Posts
+          </span>
+        </h2>
+        {/* <LinkedInPosts posts={posts} /> */}
+        {/* <iframe
+          src="https://www.linkedin.com/embed/feed/update/urn:li:share:1234567890123456789"
+          height="500"
+          width="100%"
+          frameBorder="0"
+          allowFullScreen
+          title="Exploring Redux: A Game-Changer for State Management in JavaScript Applications"
+        ></iframe> */}
 
-      <iframe
-        src="https://www.linkedin.com/embed/feed/update/urn:li:share:7247725701204770817"
-        height="1027"
-        width="504"
-        frameborder="0"
-        allowfullscreen=""
-        title="Exploring Redux: A Game-Changer for State Management in JavaScript Applications"
-      ></iframe>
+        <iframe
+          src="https://www.linkedin.com/embed/feed/update/urn:li:share:7247725701204770817"
+          // height="1027"
+          // width="504"
+          height="700"
+          width="100%"
+          frameBorder="0"
+          allowFullScreen
+          title="Exploring Redux: A Game-Changer for State Management in JavaScript Applications"
+        ></iframe>
 
-      <EmptyPage />
-    </div>
+        <EmptyPage />
+      </div>
+    </>
+    
   );
 };
 
