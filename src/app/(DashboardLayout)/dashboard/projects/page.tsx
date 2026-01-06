f466db472f25c4b9494d397e3860671dc66e5a4b
MarGoN <monondcosta@gmail.com>
2026-01-06 19:12:39 +0600

    Add: Added Projects page in dashboard.

---
+++ src/app/(DashboardLayout)/dashboard/projects/page.tsx
@@ -0,0 +1,18 @@
+import Progress from '@/components/modules/dashboard/Progress';
+import React from 'react';
+
+// meta tags
+export const metadata = {
+  title: "Projects Lists | Sokhorio Margon D' Costa",
+  description: "Projects Lists of Sokhorio Margon D' Costa",
+};
+
+const projects = () => {
+    return (
+        <div>
+            <Progress/>
+        </div>
+    );
+};
+
+export default projects;
