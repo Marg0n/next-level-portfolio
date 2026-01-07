fa8827ade906b1d1018507b20c40ef3f5e3363fa
MarGoN <monondcosta@gmail.com>
2026-01-07 19:25:36 +0600

    Add: Added Projects components to work on at a latter date.

---
+++ src/components/modules/dashboard/Projects.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div>
+            It&apos;s a list of projects I&apos;m done, doing and will do.
+        </div>
+    );
+};
+
+export default Projects;
