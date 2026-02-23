46be7c7281598bf3e467faa0391bcf97abcc5e83
MarGoN <monondcosta@gmail.com>
2026-02-23 22:53:34 +0600

    Add: Added proxy.

---
+++ src/proxy.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET!;
+
+//? This function can be marked `async` if using `await` inside
+export function proxy(req: NextRequest) {
+  const token = req.cookies.get("admin-token")?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  try {
+    jwt.verify(token, JWT_SECRET);
+    return NextResponse.next();
+  } catch {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+}
+
+export const config = {
+  matcher: "/admin/:path*",
+};
